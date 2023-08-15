79c58a60dab107c5627e6c9fb2055119c318a2ab
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-15 19:52:48 +0300

    Finish notes bottom sheet.

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -14,40 +14,45 @@ class AddNoteBottomSheet extends StatelessWidget {
         right: 20,
         left: 20,
       ),
-      child: Column(
-        children: [
-          const CustomTextField(
-            hintText: "Title",
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          const CustomTextField(
-            hintText: "Content",
-            maxLines: 6,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomMainButton(
-            text: "Add Note",
-            onPressed: () {},
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100),
-            child: CustomMainButton(
-              text: "Cancel",
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              color: const Color(0xff8D070F),
-              height: 45,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomTextField(
+              hintText: "Title",
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 24,
+            ),
+            const CustomTextField(
+              hintText: "Content",
+              maxLines: 6,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomMainButton(
+              text: "Add Note",
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: CustomMainButton(
+                text: "Cancel",
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: const Color(0xff8D070F),
+                height: 45,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
