e52865d544a455c9dd0f39fdb19639c606974b66
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-15 23:20:18 +0300

    Add register adapter to the main

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/models/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  // Function to add notes in the HIVE database
+  addNote(NoteModel note){
+
+  }
 }
