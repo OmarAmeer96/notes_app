2d22883c8a69f4ff1b9853b33770fc4f5d6c2466
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-16 21:57:23 +0300

    Format the date in the add note form

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -4,6 +4,7 @@ import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/models/note_model.dart';
 import 'custom_main_button.dart';
 import 'custom_text_field.dart';
+import 'package:intl/intl.dart';
 
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
@@ -53,7 +54,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 var noteModel = NoteModel(
                   title: title!,
                   subTitle: subTitle!,
-                  date: DateTime.now().toString(),
+                  // date:  DateTime.now().toString(),
+                  date: DateFormat('yyyy-MM-dd').format(DateTime.now()),
                   color: Colors.blue.value,
                 );
                 BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
