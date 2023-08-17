e8041301feda03aadbd2d53a0ce7b223e2f611de
Abdullah Aydoğan <aydoganabdullah34@gmail.com>
2023-08-17 21:55:41 +0300

    overflow by pixel error resolved

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -10,7 +10,7 @@ class EditNoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: EditNoteViewBody(note: note),
+      body: SingleChildScrollView(child: EditNoteViewBody(note: note)),
     );
   }
 }
