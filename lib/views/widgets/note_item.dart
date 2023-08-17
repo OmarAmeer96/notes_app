fd93573aa79b6ec664c2e7f2108f6711cc321ff1
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-17 20:10:17 +0300

    Edited teh shape of the container

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -13,6 +13,9 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color baseColor = Color(note.color);
+    Color greyColor = Colors.grey;
+    Color? blendedColor = Color.lerp(baseColor, greyColor, 0.3);
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -24,63 +27,112 @@ class NoteItem extends StatelessWidget {
           ),
         );
       },
-      child: Container(
-        padding: const EdgeInsets.only(
-          top: 24,
-          bottom: 24,
-          left: 10,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Color(note.color),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                note.title,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 27,
-                ),
+      child: Stack(
+        children: [
+          ClipPath(
+            clipper: NotePaperClipper(45),
+            child: Container(
+              padding: const EdgeInsets.only(
+                top: 24,
+                bottom: 24,
+                left: 10,
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Text(
-                  note.subTitle,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.5),
-                    fontSize: 18,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                  topLeft: Radius.circular(16),
+                ),
+                color: Color(note.color),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ListTile(
+                    title: Text(
+                      note.title,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 27,
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        note.subTitle,
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(
+                        FontAwesomeIcons.trash,
+                        color: Colors.black,
+                        size: 22,
+                      ),
+                      onPressed: () {
+                        note.delete();
+                        BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                        showErrorSnackBar(context, "Note successfully deleted");
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 28),
+                    child: Text(
+                      note.date,
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: ClipPath(
+              clipper: NotePaperClipper(45),
+              child: Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  color: blendedColor,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(16),
                   ),
                 ),
               ),
-              trailing: IconButton(
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 22,
-                ),
-                onPressed: () {
-                  note.delete();
-                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
-                  showErrorSnackBar(context, "Note successfully deleted");
-                },
-              ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 28),
-              child: Text(
-                note.date,
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 15,
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
+  }
+}
+
+class NotePaperClipper extends CustomClipper<Path> {
+  final double cutCornerSize;
+
+  NotePaperClipper(this.cutCornerSize);
+
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..lineTo(size.width - cutCornerSize, 0)
+      ..lineTo(size.width, cutCornerSize)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
